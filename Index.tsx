6e9e0d6fca11d61aa84022c25d8baccd2da6b741
Piyush Saxena <piyushsaxena168@gmail.com>
2025-07-11 13:23:39 +0530

    Create Index.tsx

---
+++ Index.tsx
@@ -0,0 +1,12 @@
+
+import VendingMachine from '../components/VendingMachine';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-slate-800">
+      <VendingMachine />
+    </div>
+  );
+};
+
+export default Index;
