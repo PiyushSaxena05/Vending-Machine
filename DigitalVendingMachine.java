34b70592442dec164961c24951ab12c0e1924d07
Piyush Saxena <piyushsaxena168@gmail.com>
2025-07-11 12:49:31 +0530

    Update DigitalVendingMachine.java

--- DigitalVendingMachine.java
+++ DigitalVendingMachine.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class DigitalVendingMachine {
     private static final String url = "jdbc:mysql://localhost:3306/db2";
     private static final String user = "root";
-    private static final String password = "PIYUSH@111WORD016";
+    private static final String password = "Password007";
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         int coffeeprice = 100;
         int coldrinkprice = 50;
