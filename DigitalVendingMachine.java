1c75aa2193692c36c99f4e0d004f111462b7c531
Piyush Saxena <piyushsaxena168@gmail.com>
2025-08-30 20:25:19 +0530

    Update DigitalVendingMachine.java

--- DigitalVendingMachine.java
+++ DigitalVendingMachine.java
@@ -15,7 +15,7 @@ public class DigitalVendingMachine {
         int juiceprice = 60;
 
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         }catch (ClassNotFoundException e){
             System.out.println(e.getMessage());
         }
@@ -314,4 +314,5 @@ public class DigitalVendingMachine {
         System.out.println("ThankYou ");
     }
 }
+
 
